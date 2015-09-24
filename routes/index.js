65754f8b21833ebe8d6f905c65efd171b9877100
Prasanna Kapate <prasanna.1616@gmail.com>
2015-09-24 11:44:43 +0530

    Added router, mongooose schema, mongo filter commands

--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,25 @@ var express = require('express');
 var router = express.Router();
 var standupCtrl = require('../controllers/standup.server.controller');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+/*Get Home Page. */
+router.get('/', function(req, res) {
+    return standupCtrl.list(req, res);
 });
 
+/*POST filter by member name - home page. */
+router.post('/',function(req,res){
+  	return standupCtrl.filterByMember(req, res);
+});
 
 /* Get New Note page. */
-router.get('/newnote', function(req,res){
-	return standupCtrl.getNote(req,res);
+router.get('/newnote', function(req, res) {
+    return standupCtrl.getNote(req, res);
 });
 
 /* Post New Note page. */
-router.post('/newnote', function(req,res){
-	return standupCtrl.create(req,res);
+router.post('/newnote', function(req, res) {
+    return standupCtrl.create(req, res);
 });
 
 
