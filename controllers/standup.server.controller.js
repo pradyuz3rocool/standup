123c0ce7f245925424ea14484a2d73c66d41a38b
Prasanna Kapate <prasanna.1616@gmail.com>
2015-09-25 11:20:27 +0530

    Added validations for the mongoose schema

--- controllers/standup.server.controller.js
+++ controllers/standup.server.controller.js
@@ -1,46 +1,74 @@
 var Standup = require('../models/standup.server.model.js');
 
-exports.list = function(req, res){
-	var query = Standup.find();
+exports.list = function(req, res) {
+    var query = Standup.find();
 
-	query.sort({createdOn: 'desc'})
-		 .limit(12)
-		 .exec(function(err, results){
-		 	res.render('index', {title:'Standup - List', notes: results });
-		 });
+    query.sort({
+            createdOn: 'desc'
+        })
+        .limit(12)
+        .exec(function(err, results) {
+            res.render('index', {
+                title: 'Standup - List',
+                notes: results
+            });
+        });
 };
 
-exports.filterByMember = function(req,res){
-	var query = Standup.find();
-	var filter = req.body.memberName;
+exports.filterByMember = function(req, res) {
+    var query = Standup.find();
+    var filter = req.body.memberName;
 
-	query.sort({createdOn:'desc'});
+    query.sort({
+        createdOn: 'desc'
+    });
 
-	if(filter.length > 0)
-	{
-		query.where({memberName:filter});
-	}
+    if (filter.length > 0) {
+        query.where({
+            memberName: filter
+        });
+    }
 
-	query.exec(function(err,results){
-		res.render('index', {title:'Standup - List', notes: results });
-	});
+    query.exec(function(err, results) {
+        res.render('index', {
+            title: 'Standup - List',
+            notes: results
+        });
+    });
 };
 
-exports.create = function(req, res){
-	var entry = new Standup({
-		memberName : req.body.memberName,
-		project : req.body.project,
-		workYesterday : req.body.workYesterday,
-		workToday : req.body.workToday,
-		impediment : req.body.impediment
-	});
+exports.create = function(req, res) {
+    var entry = new Standup({
+        memberName: req.body.memberName,
+        project: req.body.project,
+        workYesterday: req.body.workYesterday,
+        workToday: req.body.workToday,
+        impediment: req.body.impediment
+    });
 
-	entry.save();
+    //validate now...
+    entry.schema.path('memberName').validate(function(value) {
+        return value != 'None';
+    }, 'You must select a team member name.');
 
-	//redirect to home page...
-	res.redirect(301, '/');
+
+    entry.save(function(err) {
+        if (err) {
+            var errMsg = 'Sorry, there was an error saving the standup meeting notes. ' + err;
+            res.render('newnote', {
+                title: 'Standup - New note (error)',
+                message: errMsg
+            });
+        } else {
+            console.log('standup meeting note was saved!');
+            //redirected to home page to display list of notes...
+            res.redirect(301, '/');
+        }
+    });
 };
 
-exports.getNote = function(req,res){
-		res.render('newnote', {title: 'Standup -New Note'});
+exports.getNote = function(req, res) {
+    res.render('newnote', {
+        title: 'Standup -New Note'
+    });
 };
--- models/standup.server.model.js
+++ models/standup.server.model.js
@@ -1,14 +1,53 @@
 var mongoose = require('mongoose');
-var Schema   = mongoose.Schema;
+var Schema = mongoose.Schema;
 
+var memberNameValidator = [
+    function(val) {
+        return (val.length > 0 && val.toLocaleLowerCase() != 'none');
+    },
+    //custom error text...
+    'select a valid member name.'
+];
+
+var requriredStringValidator = [
+    function(val){
+        var testVal = val.trim();
+        return (testVal.length > 0);
+    },
+    //Custom error text...
+    '{PATH}  cannot be empty'
+];
 var standupSchema = new Schema({
 
-	memberName : String,
-	project : String,
-	workYesterday : String,
-	workToday : String,
-	impediment : String,
-	createdOn : {type: Date, default: Date.now}
+    memberName: {
+        type: String,
+        requrired: true,
+        validate: memberNameValidator
+    },
+    project: {
+        type: String,
+        requrired: true,
+        validate: requriredStringValidator
+    },
+    workYesterday: {
+        type: String,
+        requrired: true,
+        validate: requriredStringValidator
+    },
+    workToday: {
+        type: String,
+        requrired: true,
+        validate: requriredStringValidator
+    },
+    impediment: {
+        type: String,
+        requrired: true,
+        default: 'none'
+    },
+    createdOn: {
+        type: Date,
+        default: Date.now
+    }
 });
 
 //Export model...
